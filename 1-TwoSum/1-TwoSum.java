a95f50f07a026bb7c7870854ea58e568ca874825
KanishkaS-22 <skanishka22072006@gmail.com>
2026-04-08 20:18:08 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.java
@@ -0,0 +1,14 @@
+// Last updated: 4/8/2026, 8:18:08 PM
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> pairIdx=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int num=nums[i];
+            if(pairIdx.containsKey(target-num)){
+                return new int[]{i,pairIdx.get(target-num)};
+            }
+            pairIdx.put(num,i);
+        }
+        return new int[]{};
+    }
+}
