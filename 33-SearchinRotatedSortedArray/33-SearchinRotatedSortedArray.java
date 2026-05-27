915a8ed72ebe700c9082d9b2e90e94fc36cc6965
KanishkaS-22 <skanishka22072006@gmail.com>
2026-05-27 14:08:15 +0530

    File updated at 5/27/2026, 2:08:14 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
@@ -1,15 +1,11 @@
-// Last updated: 5/27/2026, 2:01:30 PM
+// Last updated: 5/27/2026, 2:08:14 PM
 1class Solution {
-2    public int[] searchRange(int[] nums, int target) {
-3        int f=-1,l=-1;
-4        for(int i=0;i<nums.length;i++){
-5            if(nums[i]==target){
-6                if(f==-1){
-7                    f=i;
-8                }
-9                l=i;
-10            }
-11        }
-12        return new int[]{f,l};
-13    }
-14}
+2    public boolean search(int[] nums, int target) {
+3        for(int i=0;i<nums.length;i++){
+4            if(nums[i]==target){
+5                return true;
+6            }
+7        }
+8        return false;
+9    }
+10}
