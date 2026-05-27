550ac7a96b29f0f53520c2eb88c14fc993358288
KanishkaS-22 <skanishka22072006@gmail.com>
2026-05-27 14:37:51 +0530

    File updated at 5/27/2026, 2:37:50 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.java
@@ -1,18 +1,13 @@
-// Last updated: 5/27/2026, 2:30:27 PM
+// Last updated: 5/27/2026, 2:37:50 PM
 1class Solution {
-2    public int singleNumber(int[] nums) {
-3        for(int i=0;i<nums.length;i++){
-4            int c=0;
-5            for(int j=0;j<nums.length;j++){
-6                if(nums[i]==nums[j]){
-7                   c++;
-8                }
-9            }
-10        
-11        if(c==1){
-12           return nums[i];
-13        }
-14        }
-15        return -1;
-16    }
-17}
+2    public int findMin(int[] nums) {
+3        int min=nums[0];
+4        for(int i=0;i<nums.length;i++){
+5           
+6            if(nums[i]<min){
+7                min=nums[i];
+8            }
+9        }
+10     return min;   
+11    }
+12}
