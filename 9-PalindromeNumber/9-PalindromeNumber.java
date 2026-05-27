804457b52da304b49a80c616dba388169d4399d0
KanishkaS-22 <skanishka22072006@gmail.com>
2026-05-27 15:58:44 +0530

    File updated at 5/27/2026, 3:58:43 PM

--- 9-PalindromeNumber/9-PalindromeNumber.java
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -1,15 +1,15 @@
-// Last updated: 4/8/2026, 8:18:07 PM
-class Solution {
-    public boolean isPalindrome(int x) {
-      if(x<0){
-        return false;
-      }
-      int rev=0;
-      int y=x;
-      while(x>0){
-        rev=(rev*10)+(x%10);
-        x/=10;
-      }
-      return rev==y;
-    }
-}
+// Last updated: 5/27/2026, 3:58:43 PM
+1class Solution {
+2    public String reverseWords(String s) {
+3        s=s.trim();
+4        String arr[]=s.split("\\s+");
+5        String ans="";
+6        for(int i=arr.length-1;i>=0;i--){
+7            ans=ans+arr[i];
+8            if(i!=0){
+9                ans=ans+" ";
+10            }
+11        }
+12        return ans;
+13    }
+14}
