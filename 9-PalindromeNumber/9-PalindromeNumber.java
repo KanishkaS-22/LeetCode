d8259a82fc9f5896bdfe52b5b4477afc9062fe9a
KanishkaS-22 <skanishka22072006@gmail.com>
2026-05-27 16:23:13 +0530

    File updated at 5/27/2026, 4:23:13 PM

--- 9-PalindromeNumber/9-PalindromeNumber.java
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -1,50 +1,7 @@
-// Last updated: 5/27/2026, 4:18:54 PM
-1// class Solution {
-2//     public int countPrimes(int n) {
-3//     int cp=0;
-4//         for(int i=2;i<n;i++){
-5//             int c=0;
-6//             for(int j=1;j<=i;j++){
-7//                 if(i%j==0){
-8//                     c++;
-9//                 }
-10//             }
-11//         if(c==2){
-12//             cp++;
-13//         }
-14//         }
-15// return cp;
-16//     }
-17// }
-18class Solution {
-19
-20    public int countPrimes(int n) {
-21
-22        boolean[] prime = new boolean[n];
-23
-24        for(int i = 0; i < n; i++) {
-25            prime[i] = true;
-26        }
-27
-28        for(int i = 2; i * i < n; i++) {
-29
-30            if(prime[i]) {
-31
-32                for(int j = i * i; j < n; j += i) {
-33                    prime[j] = false;
-34                }
-35            }
-36        }
-37
-38        int count = 0;
-39
-40        for(int i = 2; i < n; i++) {
-41
-42            if(prime[i]) {
-43                count++;
-44            }
-45        }
-46
-47        return count;
-48    }
-49}
+// Last updated: 5/27/2026, 4:23:13 PM
+1class Solution {
+2    public int findKthLargest(int[] nums, int k) {
+3        Arrays.sort(nums);
+4        return nums[nums.length-k];
+5    }
+6}
