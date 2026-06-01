8fcf2e753369c425376f4de9f61fdc206f558d9a
KanishkaS-22 <skanishka22072006@gmail.com>
2026-06-01 17:10:13 +0530

    File updated at 6/1/2026, 5:10:14 PM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.java
@@ -1,11 +1,15 @@
-// Last updated: 6/1/2026, 5:02:24 PM
+// Last updated: 6/1/2026, 5:10:14 PM
 1class Solution {
-2    public void merge(int[] nums1, int m, int[] nums2, int n) {
-3       int i=m;
-4       for(int j=0;j<n;j++){
-5        nums1[i]=nums2[j];
-6        i++;
-7       }
-8       Arrays.sort(nums1);
-9    }
-10}
+2    public int maxProfit(int[] prices) {
+3        int b=prices[0];
+4        int p=0;
+5        for(int j=1;j<prices.length;j++){
+6            if(prices[j]<b){
+7                b=prices[j];
+8            }else if(prices[j]-b>p){
+9                p=prices[j]-b;
+10            }
+11        }
+12        return p;
+13    }
+14}
