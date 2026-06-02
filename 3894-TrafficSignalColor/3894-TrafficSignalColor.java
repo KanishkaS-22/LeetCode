071b303b4bd4e5a530807759a5f9efd06113d92e
KanishkaS-22 <skanishka22072006@gmail.com>
2026-06-02 14:55:42 +0530

    Create 3894-TrafficSignalColor

---
+++ 3894-TrafficSignalColor/3894-TrafficSignalColor.java
@@ -0,0 +1,14 @@
+// Last updated: 6/2/2026, 2:55:43 PM
+1class Solution {
+2    public String trafficSignal(int timer) {
+3        if(timer==0){
+4            return "Green";
+5        }else if(timer==30){
+6            return "Orange";
+7        }else if(timer>30&&timer<=90){
+8            return "Red";
+9        }else{
+10            return "Invalid";
+11        }
+12    }
+13}
