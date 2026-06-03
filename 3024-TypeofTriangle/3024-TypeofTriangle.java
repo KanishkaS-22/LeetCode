79d7c186f6781c180d02a3551fa87477b148e87b
KanishkaS-22 <skanishka22072006@gmail.com>
2026-06-03 14:32:33 +0530

    File updated at 6/3/2026, 2:32:33 PM

--- 3024-TypeofTriangle/3024-TypeofTriangle.java
+++ 3024-TypeofTriangle/3024-TypeofTriangle.java
@@ -1,23 +1,22 @@
-// Last updated: 6/3/2026, 2:30:42 PM
+// Last updated: 6/3/2026, 2:32:33 PM
 1class Solution {
 2    public String triangleType(int[] nums) {
 3        for(int i=0;i<nums.length;i++){
 4            int n1=nums[i];
 5            int n2=nums[i+1];
 6            int n3=nums[i+2];
-7            int r=0;
-8            if((n1+n2>n3)&&(n1+n3>n2)&&(n2+n3>n1)){
-9            if(n1==n2&&n1==n3){
-10                return "equilateral";
-11            }else if(n1==n2||n1==n3||n2==n3){
-12                return "isosceles";
-13            }else if((n1+n2>n3)&&(n1+n3>n2)&&(n2+n3>n1)){
-14                return "scalene";
-15            }
-16            break;
-17        }
-18        return "none";
-19        }
-20        return "none";
-21    }
-22}
+7            if((n1+n2>n3)&&(n1+n3>n2)&&(n2+n3>n1)){
+8            if(n1==n2&&n1==n3){
+9                return "equilateral";
+10            }else if(n1==n2||n1==n3||n2==n3){
+11                return "isosceles";
+12            }else if((n1+n2>n3)&&(n1+n3>n2)&&(n2+n3>n1)){
+13                return "scalene";
+14            }
+15            break;
+16        }
+17        return "none";
+18        }
+19        return "none";
+20    }
+21}
