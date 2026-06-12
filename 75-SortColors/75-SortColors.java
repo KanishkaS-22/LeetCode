b8767caaf258ac455fcd9819fd92fec0e493b6a1
KANI <skanishka22072006@gmail.com>
2026-06-12 13:54:24 +0530

    File updated at 6/12/2026, 1:54:24 PM

--- 75-SortColors/75-SortColors.java
+++ 75-SortColors/75-SortColors.java
@@ -1,16 +1,15 @@
-// Last updated: 6/12/2026, 1:52:49 PM
+// Last updated: 6/12/2026, 1:54:24 PM
 1class Solution {
-2     public void sortColors(int[] arr) {
-3        // code here
-4        int n=arr.length;
-5        for(int i=0;i<n;i++){
-6            for(int j=0;j<n-i-1;j++){
-7                if(arr[j]>arr[j+1]){
-8                    int temp=arr[j];
-9                    arr[j]=arr[j+1];
-10                    arr[j+1]=temp;
-11                }
-12            }
-13        }
-14    }
-15}
+2    public void sortColors(int[] arr) {
+3        int n=arr.length;
+4        for(int i=0;i<n;i++){
+5            for(int j=0;j<n-i-1;j++){
+6                if(arr[j]>arr[j+1]){
+7                    int temp=arr[j];
+8                    arr[j]=arr[j+1];
+9                    arr[j+1]=temp;
+10                }
+11            }
+12        }
+13    }
+14}
