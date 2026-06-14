bfbf9e52e12f4bc6469fd23b1798e17530c334b6
KANI <skanishka22072006@gmail.com>
2026-06-14 22:22:02 +0530

    File updated at 6/14/2026, 10:22:02 PM

--- 724-FindPivotIndex/724-FindPivotIndex.java
+++ 724-FindPivotIndex/724-FindPivotIndex.java
@@ -1,20 +1,20 @@
-// Last updated: 4/8/2026, 8:16:43 PM
-class Solution {
-    public int pivotIndex(int[] nums) {
-        int n=nums.length;
-        int []ps=new int[n];
-        int []ss=new int[n];
-        ps[0]=nums[0];
-        for(int i=1;i<n;i++){
-            ps[i]=nums[i]+ps[i-1];
-        }
-        ss[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            ss[i]=nums[i]+ss[i+1];
-        }
-        for(int i=0;i<n;i++){
-           if(ps[i]==ss[i]) return i;
-        }
-         return-1;
-    }
-}
+// Last updated: 6/14/2026, 10:22:02 PM
+1class Solution {
+2    public int pivotIndex(int[] nums) {
+3         int n=nums.length;
+4        int []ps=new int[n];
+5        int []ss=new int[n];
+6        ps[0]=nums[0];
+7        for(int i=1;i<n;i++){
+8            ps[i]=nums[i]+ps[i-1];
+9        }
+10        ss[n-1]=nums[n-1];
+11        for(int i=n-2;i>=0;i--){
+12            ss[i]=nums[i]+ss[i+1];
+13        }
+14        for(int i=0;i<n;i++){
+15           if(ps[i]==ss[i]) return i;
+16        }
+17         return-1;
+18    }
+19}
