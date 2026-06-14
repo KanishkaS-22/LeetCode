376e66c378f1e8ee8408388f28901abb02f9bd0b
KANI <skanishka22072006@gmail.com>
2026-06-14 22:23:34 +0530

    File updated at 6/14/2026, 10:23:34 PM

--- 507-PerfectNumber/507-PerfectNumber.java
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -1,18 +1,18 @@
-// Last updated: 4/8/2026, 8:16:50 PM
-class Solution {
-    public boolean checkPerfectNumber(int num) {
-         int temp = num ;
-        int sum = 0;
-        for(int i = 1 ; i <= num/2 ; i++){
-            if(num%i == 0){
-                sum += i;
-            }
-        }
-        if(sum == temp){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-}
+// Last updated: 6/14/2026, 10:23:34 PM
+1class Solution {
+2    public boolean checkPerfectNumber(int num) {
+3        int temp = num ;
+4        int sum = 0;
+5        for(int i = 1 ; i <= num/2 ; i++){
+6            if(num%i == 0){
+7                sum += i;
+8            }
+9        }
+10        if(sum == temp){
+11            return true;
+12        }
+13        else{
+14            return false;
+15        }
+16    }
+17}
