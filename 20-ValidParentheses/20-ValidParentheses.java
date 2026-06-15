2345904a0ca9167b77f5e8e0525da9d060dcb537
KANI <skanishka22072006@gmail.com>
2026-06-15 09:10:36 +0530

    File updated at 6/15/2026, 9:10:35 AM

--- 20-ValidParentheses/20-ValidParentheses.java
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -1,15 +1,15 @@
-// Last updated: 4/8/2026, 8:18:01 PM
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-        for(char ch: s.toCharArray()){
-            if(ch=='(' || ch=='[' || ch=='{') st.push(ch);
-            else{
-                if(st.isEmpty()==true) return false;
-                if(st.peek()=='(' && ch==')' || st.peek()=='[' && ch==']' || st.peek()=='{' && ch=='}') st.pop();
-                else return false;
-            }
-        }
-        return st.isEmpty();
-    }
-}
+// Last updated: 6/15/2026, 9:10:35 AM
+1class Solution {
+2    public boolean isValid(String s) {
+3       Stack<Character> st = new Stack<>();
+4        for(char ch: s.toCharArray()){
+5            if(ch=='(' || ch=='[' || ch=='{') st.push(ch);
+6            else{
+7                if(st.isEmpty()==true) return false;
+8                if(st.peek()=='(' && ch==')' || st.peek()=='[' && ch==']' || st.peek()=='{' && ch=='}') st.pop();
+9                else return false;
+10            }
+11        }
+12        return st.isEmpty(); 
+13    }
+14}
