a21741fba65d7e11a8c38b0e74f4ca7f70cf7757
KANI <skanishka22072006@gmail.com>
2026-06-16 11:50:34 +0530

    File updated at 6/16/2026, 11:50:34 AM

--- 2119-ANumberAfteraDoubleReversal/2119-ANumberAfteraDoubleReversal.java
+++ 2119-ANumberAfteraDoubleReversal/2119-ANumberAfteraDoubleReversal.java
@@ -1,6 +1,19 @@
-// Last updated: 6/16/2026, 11:41:28 AM
+// Last updated: 6/16/2026, 11:50:34 AM
 1class Solution {
 2    public boolean isSameAfterReversals(int num) {
-3       return num==0||num%10!=0;
-4    }
-5}
+3       int t1=num;
+4       int r1=0;
+5       while(num>0){
+6        int d=num%10;
+7        r1=r1*10+d;
+8        num=num/10;
+9       }
+10       int r2=0;
+11       while(r1>0){
+12        int d2=r1%10;
+13        r2=r2*10+d2;
+14        r1=r1/10;
+15       }
+16       return r2==t1;
+17    }
+18}
