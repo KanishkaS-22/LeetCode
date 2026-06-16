03f2dd6c750e9404eacc51ef4e7872051cd9c058
KANI <skanishka22072006@gmail.com>
2026-06-16 12:11:16 +0530

    File updated at 6/16/2026, 12:11:16 PM

--- 933-NumberofRecentCalls/933-NumberofRecentCalls.java
+++ 933-NumberofRecentCalls/933-NumberofRecentCalls.java
@@ -1,14 +1,10 @@
-// Last updated: 6/16/2026, 11:56:26 AM
+// Last updated: 6/16/2026, 12:11:16 PM
 1class Solution {
-2    public int numIdenticalPairs(int[] nums) {
-3         int count=0;
-4        for(int i=0;i<nums.length;i++){
-5            for(int j=0;j<nums.length;j++){
-6                if(nums[i]==nums[j]&&i<j){
-7                    count++;
-8                }
-9            }
-10        }
-11        return count;
-12    }
-13}
+2    public int[] sortedSquares(int[] nums) {
+3        for(int i=0;i<nums.length;i++){
+4            nums[i]=nums[i]*nums[i];
+5        }
+6        Arrays.sort(nums);
+7        return nums;
+8    }
+9}
