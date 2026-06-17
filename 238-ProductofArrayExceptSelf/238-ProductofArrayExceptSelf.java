a659c45b10b3c9c0d673f6aa9426d275cf7597c1
KANI <skanishka22072006@gmail.com>
2026-06-17 12:11:44 +0530

    File updated at 6/17/2026, 12:11:46 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,8 +1,12 @@
-// Last updated: 6/17/2026, 12:09:02 PM
+// Last updated: 6/17/2026, 12:11:46 PM
 1class Solution {
-2    public int majorityElement(int[] nums) {
-3        Arrays.sort(nums);
-4        int n=nums.length;
-5        return nums[n/2];
-6    }
-7}
+2    public boolean containsDuplicate(int[] nums) {
+3       Arrays.sort(nums);
+4       for(int i=1;i<nums.length;i++){
+5        if(nums[i]==nums[i-1]){
+6            return true;
+7        }
+8       }
+9       return false;
+10    }
+11}
