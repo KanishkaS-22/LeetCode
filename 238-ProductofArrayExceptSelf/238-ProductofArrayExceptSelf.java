b8d0e21fe9916d5e61adfdb1b13c4c65a12192f1
KANI <skanishka22072006@gmail.com>
2026-06-17 12:09:00 +0530

    File updated at 6/17/2026, 12:09:02 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,15 +1,8 @@
-// Last updated: 6/17/2026, 12:05:03 PM
+// Last updated: 6/17/2026, 12:09:02 PM
 1class Solution {
-2    public int maxProfit(int[] prices) {
-3        int b=prices[0];
-4        int p=0;
-5        for(int j=1;j<prices.length;j++){
-6            if(prices[j]<b){
-7                b=prices[j];
-8            }else if(prices[j]-b>p){
-9                p=prices[j]-b;
-10            }
-11        }
-12        return p;
-13    }
-14}
+2    public int majorityElement(int[] nums) {
+3        Arrays.sort(nums);
+4        int n=nums.length;
+5        return nums[n/2];
+6    }
+7}
