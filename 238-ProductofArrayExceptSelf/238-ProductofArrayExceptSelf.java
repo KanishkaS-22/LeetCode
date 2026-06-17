0c21c46158a9a41703b3196917dad05b19c057a0
KANI <skanishka22072006@gmail.com>
2026-06-17 14:22:37 +0530

    File updated at 6/17/2026, 2:22:39 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.java
@@ -1,11 +1,13 @@
-// Last updated: 6/17/2026, 2:17:42 PM
+// Last updated: 6/17/2026, 2:22:39 PM
 1class Solution {
-2    public int firstUniqChar(String s) {
-3        int[] count = new int[26];
-4        for(char i: s.toCharArray()) count[i -'a']++;
-5        for(int i = 0; i < s.length(); i++) {
-6            if(count[s.charAt(i) - 'a'] == 1) return i;
-7        }
-8        return -1;
-9    }
-10}
+2    public boolean isAnagram(String s, String t) {
+3        char[]arr1=s.toCharArray();
+4        char[]arr2=t.toCharArray();
+5        Arrays.sort(arr1);
+6        Arrays.sort(arr2);
+7        if(Arrays.equals(arr1,arr2)){
+8            return true;
+9        }
+10        return false;
+11    }
+12}
