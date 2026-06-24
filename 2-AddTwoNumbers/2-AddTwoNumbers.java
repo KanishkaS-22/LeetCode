8cf4094653da0c9fc8d53efdb43073861149ca0e
KANI <skanishka22072006@gmail.com>
2026-06-24 23:52:16 +0530

    File updated at 6/24/2026, 11:52:16 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,22 +1,23 @@
-// Last updated: 6/24/2026, 11:51:55 PM
+// Last updated: 6/24/2026, 11:52:16 PM
 1class Solution {
-2    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-3        ListNode dummyHead = new ListNode(0);
-4        ListNode curr = dummyHead;
-5        int carry = 0;
-6
-7        while (l1 != null || l2 != null || carry != 0) {
-8            int x = (l1 != null) ? l1.val : 0;
-9            int y = (l2 != null) ? l2.val : 0;
-10            int sum = carry + x + y;
-11
-12            carry = sum / 10;
-13            curr.next = new ListNode(sum % 10);
-14            curr = curr.next;
-15
-16            if (l1 != null) l1 = l1.next;
-17            if (l2 != null) l2 = l2.next;
-18        }
-19        return dummyHead.next;
-20    }
-21}
+2    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+3        int[]nums3=new int[nums1.length+nums2.length];
+4        for(int i=0;i<nums1.length;i++){
+5            nums3[i]=nums1[i];
+6        }
+7        for(int i=0;i<nums2.length;i++){
+8            nums3[nums1.length+i]=nums2[i];
+9        }
+10        double k=0;
+11        Arrays.sort(nums3);
+12            if(nums3.length%2!=0){
+13              int sum=nums3.length/2;
+14              k=nums3[sum];
+15            }else{
+16              int sum2=nums3.length/2;
+17              k=(nums3[sum2]+nums3[sum2-1])/2.0;
+18            }
+19            return k;
+20        }
+21    }
+22
