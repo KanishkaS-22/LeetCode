4c3d7cef7bb93b7e8c65e73e1936c52e96d351b8
KANI <skanishka22072006@gmail.com>
2026-07-01 11:58:21 +0530

    File updated at 7/1/2026, 11:58:21 AM

--- 268-MissingNumber/268-MissingNumber.java
+++ 268-MissingNumber/268-MissingNumber.java
@@ -1,10 +1,10 @@
-// Last updated: 4/8/2026, 8:17:04 PM
-class Solution {
-    public int missingNumber(int[] nums) {
-        int res=nums.length;
-        for(int i=0;i<nums.length;i++){
-            res+=i-nums[i];
-        }
-        return res;
-    }
-}
+// Last updated: 7/1/2026, 11:58:21 AM
+1class Solution {
+2    public int missingNumber(int[] nums) {
+3        int n=nums.length;
+4        for(int i=0;i<nums.length;i++){
+5            n+=i-nums[i];
+6        }
+7        return n;
+8    }
+9}
