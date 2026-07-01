d6e2c929ceda4d9b8a348ed5b9e8a6c396837bfc
KANI <skanishka22072006@gmail.com>
2026-07-01 14:09:36 +0530

    Create

---
+++ problem-1782895175680/problem-1782895175680.java
@@ -0,0 +1,9 @@
+// Last updated: 7/1/2026, 2:09:35 PM
+1class Solution {
+2    public boolean canWinNim(int n) {
+3        if(n%4!=0){
+4            return true;
+5        }
+6        return false;
+7    }
+8}
