e7902612f11ac0d82e5e61e9e57deedb2d61387f
KANI <skanishka22072006@gmail.com>
2026-08-13 10:15:18 +0530

    Create 4264-FirstMatchingCharacterFromBothEnds

---
+++ 4264-FirstMatchingCharacterFromBothEnds/4264-FirstMatchingCharacterFromBothEnds.java
@@ -0,0 +1,14 @@
+// Last updated: 8/13/2026, 10:15:18 AM
+class Solution {
+    public int firstMatchingIndex(String s) {
+        int l=0,h=s.length()-1;
+        while(l<=h){
+            if(s.charAt(l)==s.charAt(h)){
+                return l;
+            }
+                    l++;
+                    h--;
+                }
+        return -1;
+    }
+}
