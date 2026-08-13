65594cd98c7aaae27df712477e392b3e92b47199
KANI <skanishka22072006@gmail.com>
2026-08-13 10:17:57 +0530

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,13 @@
+// Last updated: 8/13/2026, 10:17:57 AM
+class Solution {
+    public char findTheDifference(String s, String t) {
+       char ans=0;
+       for(int i=0;i<s.length();i++){
+        ans^=s.charAt(i);
+       }
+       for(int j=0;j<t.length();j++){
+        ans^=t.charAt(j);
+       }
+       return ans;
+    }
+}
