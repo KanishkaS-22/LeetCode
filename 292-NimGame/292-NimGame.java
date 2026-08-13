f2ed0ec33f0e08b915ff5f208c3a79e65a9418fe
KANI <skanishka22072006@gmail.com>
2026-08-13 10:18:16 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 8/13/2026, 10:18:15 AM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4!=0){
+            return true;
+        }
+        return false;
+    }
+}
