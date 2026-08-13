6cb7efdd98bdcd8d0ffc837df1f9c6d8b1624a5f
KANI <skanishka22072006@gmail.com>
2026-08-13 10:19:18 +0530

    Create 153-FindMinimumInRotatedSortedArray

---
+++ 153-FindMinimumInRotatedSortedArray/153-FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,13 @@
+// Last updated: 8/13/2026, 10:19:18 AM
+class Solution {
+    public int findMin(int[] nums) {
+        int min=nums[0];
+        for(int i=0;i<nums.length;i++){
+           
+            if(nums[i]<min){
+                min=nums[i];
+            }
+        }
+     return min;   
+    }
+}
