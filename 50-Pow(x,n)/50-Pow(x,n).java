ffcc52c27b58da15891a142fd3dc974c3b9c72ae
Kanishka S <skanishka22072006@gmail.com>
2026-09-02 11:59:22 +0530

    File updated at 9/2/2026, 11:59:22 AM

--- 50-Pow(x,n)/50-Pow(x,n).java
+++ 50-Pow(x,n)/50-Pow(x,n).java
@@ -1,23 +1,24 @@
-// Last updated: 9/2/2026, 11:53:03 AM
+// Last updated: 9/2/2026, 11:59:22 AM
 1class Solution {
-2    public String addBinary(String a, String b) {
-3        StringBuilder ans=new StringBuilder();
-4        int i=a.length()-1;
-5        int j=b.length()-1;
-6        int c=0;
-7        while(i>=0||j>=0||c!=0){
-8            int sum=c;
-9            if(i>=0){
-10                sum+=a.charAt(i)-'0';
-11                i--;
-12            }
-13            if(j>=0){
-14                sum+=b.charAt(j)-'0';
-15                j--;
-16            }
-17            ans.append(sum%2);
-18            c=sum/2;
-19        }
-20        return ans.reverse().toString();
-21    }
-22}
+2    public void setZeroes(int[][] matrix) {
+3        int rows=matrix.length;
+4        int cols=matrix[0].length;
+5        boolean[]row=new boolean[rows];
+6        boolean[]col=new boolean[cols];
+7        for(int i=0;i<rows;i++){
+8            for(int j=0;j<cols;j++){
+9                if(matrix[i][j]==0){
+10                    row[i]=true;
+11                    col[j]=true;
+12                }
+13            }
+14        }
+15        for(int i=0;i<rows;i++){
+16            for(int j=0;j<cols;j++){
+17                if(row[i]||col[j]){
+18                    matrix[i][j]=0;
+19                }
+20            }
+21        }
+22    }
+23}
