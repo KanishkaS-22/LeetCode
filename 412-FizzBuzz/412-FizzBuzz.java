afd7b47d8c568612c7a7904830fc33bb09eee16b
Kanishka S <skanishka22072006@gmail.com>
2026-09-02 10:22:14 +0530

    File updated at 9/2/2026, 10:22:14 AM

--- 412-FizzBuzz/412-FizzBuzz.java
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -1,15 +1,9 @@
-// Last updated: 9/2/2026, 10:17:17 AM
+// Last updated: 9/2/2026, 10:22:14 AM
 1class Solution {
-2    public int numberOfSteps(int num) {
-3        int c=0;
-4        while(num!=0){
-5            if(num%2==0){
-6                num=num/2;
-7            }else{
-8                num=num-1;
-9            }
-10            c++;
-11        }
-12        return c;
-13    }
-14}
+2    public int[] runningSum(int[] nums) {
+3        for(int i=1;i<nums.length;i++){
+4            nums[i]=nums[i-1]+nums[i];
+5        }
+6        return nums;
+7    }
+8}
