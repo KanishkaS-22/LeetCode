aa9af66a7bfc959146804f7d5d2cc6da86f7ba7a
Kanishka S <skanishka22072006@gmail.com>
2026-09-02 10:29:02 +0530

    Create

---
+++ problem-1788325142655/problem-1788325142655.java
@@ -0,0 +1,14 @@
+// Last updated: 9/2/2026, 10:29:02 AM
+1class Solution {
+2    public int[] getConcatenation(int[] nums) {
+3        int n=nums.length;
+4        int[]num2=new int[n+n];
+5        for(int i=0;i<n;i++){
+6            num2[i]=nums[i];
+7        }
+8        for(int i=0;i<n;i++){
+9            num2[i+n]=nums[i];
+10        }
+11        return num2;
+12    }
+13}
