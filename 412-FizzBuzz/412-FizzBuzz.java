3c353091dd877caae287972505efe8564f906040
Kanishka S <skanishka22072006@gmail.com>
2026-09-02 10:25:39 +0530

    File updated at 9/2/2026, 10:25:39 AM

--- 412-FizzBuzz/412-FizzBuzz.java
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -1,9 +1,14 @@
-// Last updated: 9/2/2026, 10:22:14 AM
+// Last updated: 9/2/2026, 10:25:39 AM
 1class Solution {
-2    public int[] runningSum(int[] nums) {
-3        for(int i=1;i<nums.length;i++){
-4            nums[i]=nums[i-1]+nums[i];
-5        }
-6        return nums;
-7    }
-8}
+2    public int maximumWealth(int[][] accounts) {
+3        int res=0;
+4        for(int i=0;i<accounts.length;i++){
+5            int t=0;
+6            for(int j=0;j<accounts[i].length;j++){
+7                t+=accounts[i][j];
+8            }
+9            res=Math.max(t,res);
+10        }
+11        return res;
+12    }
+13}
