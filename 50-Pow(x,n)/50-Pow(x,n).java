91616349eb4644d0307fb596c10fd4aafb8f03e7
Kanishka S <skanishka22072006@gmail.com>
2026-09-02 11:53:03 +0530

    File updated at 9/2/2026, 11:53:03 AM

--- 50-Pow(x,n)/50-Pow(x,n).java
+++ 50-Pow(x,n)/50-Pow(x,n).java
@@ -1,19 +1,23 @@
-// Last updated: 9/2/2026, 11:45:42 AM
+// Last updated: 9/2/2026, 11:53:03 AM
 1class Solution {
-2    public double myPow(double x, int n) {
-3        long pow=n;
-4        if(pow<0){
-5            x=1/x;
-6            pow=-pow;
-7        }
-8        double ans=1;
-9        while(pow>0){
-10            if(pow%2==1){
-11                ans=ans*x;
+2    public String addBinary(String a, String b) {
+3        StringBuilder ans=new StringBuilder();
+4        int i=a.length()-1;
+5        int j=b.length()-1;
+6        int c=0;
+7        while(i>=0||j>=0||c!=0){
+8            int sum=c;
+9            if(i>=0){
+10                sum+=a.charAt(i)-'0';
+11                i--;
 12            }
-13            x=x*x;
-14            pow=pow/2;
-15        }
-16        return ans;
-17    }
-18}
+13            if(j>=0){
+14                sum+=b.charAt(j)-'0';
+15                j--;
+16            }
+17            ans.append(sum%2);
+18            c=sum/2;
+19        }
+20        return ans.reverse().toString();
+21    }
+22}
