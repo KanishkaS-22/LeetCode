419cdcc4a4703ef7584543e2037453c90a4d337f
Kanishka S <skanishka22072006@gmail.com>
2026-09-02 12:31:33 +0530

    File updated at 9/2/2026, 12:31:33 PM

--- 182-DuplicateEmails/182-DuplicateEmails.java
+++ 182-DuplicateEmails/182-DuplicateEmails.java
@@ -1,16 +1,21 @@
-// Last updated: 9/2/2026, 12:27:00 PM
+// Last updated: 9/2/2026, 12:31:33 PM
 1class Solution {
-2    public boolean containsNearbyDuplicate(int[] nums, int k) {
-3        HashMap<Integer,Integer>map=new HashMap<>();
-4        for(int i=0;i<nums.length;i++){
-5            if(map.containsKey(nums[i])){
-6                int ind=map.get(nums[i]);
-7                if(i-ind<=k){
-8                    return true;
-9                }
-10            }
-11            map.put(nums[i],i);
-12        }
-13        return false;
-14    }
-15}
+2    public int countDigitOne(int n) {
+3        long f=1;
+4        int ct=0;
+5        while(f<=n){
+6            long l=n%f;
+7            long c=(n/f)%10;
+8            long h=n/(f*10);
+9            if(c==0){
+10              ct+=h*f;
+11            }else if(c==1){
+12                ct+=h*f+l+1;
+13            }else{
+14                ct+=(h+1)*f;
+15            }
+16            f*=10;
+17        }
+18        return ct;
+19    }
+20}
